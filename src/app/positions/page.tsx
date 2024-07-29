82e7765382874a052747fcdbe6a2e0e5b1463b98
boris <boris@ascendfinance.xyz>
2024-07-29 16:55:56 +0500

    positions page completed

--- src/app/positions/page.tsx
+++ src/app/positions/page.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
+import { useState } from "react";
+
 import Image from "next/image";
 
 import PositionCard from "@/components/PositionCard";
@@ -7,7 +10,8 @@ import ezETHIcon from "../../../public/icons/ezETH.svg";
 import postionsIcon from "../../../public/icons/positionsIcon.svg";
 import weETHIcon from "../../../public/icons/weETH.svg";
 
-const page = () => {
+const Page = () => {
+  const [positionsAvailable, setpositionsAvailable] = useState(true);
   return (
     <div className="flex items-center justify-center min-h-full w-full">
       <div className="bg-baseColor rounded-3xl w-[95%] mt-36 mb-22 sm:my-[10px] sm:w-[70%] xl:w-[80%] 2xl:w-[40%]">
@@ -29,33 +33,43 @@ const page = () => {
           </p>
         </div>
         <hr className="border-lightGray2" />
-        <div className="py-6 px-12 flex-wrap lg:flex-nowrap flex gap-8">
-          <PositionCard
-            icon={weETHIcon}
-            symbol="weETH"
-            tokenName="EtherFi Restaked Ether"
-            collateral="1.500 weETH"
-            mintedValue="2930.28 BLUE"
-            collateralRatio="129%"
-            liquidationPrice="$3029.39 / ETH"
-            withdrawAction={async () => {}}
-            repayAction={async () => {}}
-          />
-          <PositionCard
-            icon={ezETHIcon}
-            symbol="ezETH"
-            tokenName="Renzo Restaked Ether"
-            collateral="3.543 ezETH"
-            mintedValue="12 930.93 BLUE"
-            collateralRatio="339%"
-            liquidationPrice="$3029.39 / ETH"
-            withdrawAction={async () => {}}
-            repayAction={async () => {}}
-          />
+        <div className="py-12 px-12 flex-wrap lg:flex-nowrap flex gap-8">
+          {!positionsAvailable ? (
+            <div className="h-[250px] w-full flex items-center justify-center">
+              <p className="font-bold text-[18px] leading-[36px]">
+                You currently have no positions.
+              </p>
+            </div>
+          ) : (
+            <>
+              <PositionCard
+                icon={weETHIcon}
+                symbol="weETH"
+                tokenName="EtherFi Restaked Ether"
+                collateral="1.500 weETH"
+                mintedValue="2930.28 BLUE"
+                collateralRatio="129%"
+                liquidationPrice="$3029.39 / ETH"
+                withdrawAction={async () => {}}
+                repayAction={async () => {}}
+              />
+              <PositionCard
+                icon={ezETHIcon}
+                symbol="ezETH"
+                tokenName="Renzo Restaked Ether"
+                collateral="3.543 ezETH"
+                mintedValue="12 930.93 BLUE"
+                collateralRatio="339%"
+                liquidationPrice="$3029.39 / ETH"
+                withdrawAction={async () => {}}
+                repayAction={async () => {}}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
