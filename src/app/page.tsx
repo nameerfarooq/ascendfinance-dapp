4a38802adf48081762d0b9a0b7994da38407cf04
boris <boris@ascendfinance.xyz>
2024-07-29 14:01:21 +0500

    vaults cards updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
+import { useEffect } from "react";
+
 import Image from "next/image";
 import { useDispatch } from "react-redux";
 
@@ -12,18 +14,20 @@ import weETHIcon from "../../public/icons/weETH.svg";
 
 const MintPage = () => {
   const dispatch = useDispatch();
-  const setLoaderTrue = async () => {
-    dispatch(
-      setLoader({
-        loading: true,
-        text1: "Minting",
-        text2: "123123 Blue",
-      }),
-    );
-  };
-  setLoaderTrue();
+  useEffect(() => {
+    const setLoaderTrue = async () => {
+      dispatch(
+        setLoader({
+          loading: true,
+          text1: "Minting",
+          text2: "123123 Blue",
+        }),
+      );
+    };
+    // setLoaderTrue();
+  }, []);
   return (
-    <div className="w-[85%]  2xl:w-[70%] mx-auto mt-[150px]">
+    <div className="w-[85%]  2xl:w-[50%] mx-auto mt-[150px]">
       <div className="px-5 flex gap-6 items-center">
         <Image className="brightness-0 invert" src={vaultsIcon} alt="Vaults Icon" width={24} />
         <p className="leading-[60px] font-bold text-[30px] text-whtie">Vaults</p>
@@ -32,7 +36,7 @@ const MintPage = () => {
         Info about vaults and available LSTs etc. More coming in the future.
       </p>
       <hr className="border-[#647594] w-full my-12" />
-      <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-12 grid-cols-1 sm:grid-cols-2">
         <VaultCard
           iconImg={weETHIcon}
           symbol="weETH"
@@ -42,6 +46,8 @@ const MintPage = () => {
           minCollateralRation="130%"
           apr="5.99% - 8.99%"
           btnText="Choose weETH"
+          learnMoreLink={"/"}
+          infoSymbol="Renzo"
           btnAction={() => {}}
         />
         <VaultCard
@@ -51,8 +57,10 @@ const MintPage = () => {
           tvl="$328.34k"
           mintedBlue="299.99k/20.00m"
           minCollateralRation="130%"
-          apr="2.99% - 3.99%"
+          apr="2.99%-3.99%"
           btnText="Choose ezETH"
+          learnMoreLink={"/"}
+          infoSymbol="Ether.fi"
           btnAction={() => {}}
         />
       </div>
--- src/components/VaultCard.tsx
+++ src/components/VaultCard.tsx
@@ -2,6 +2,8 @@
 import React, { type ReactEventHandler } from "react";
 
 import Image from "next/image";
+import Link from "next/link";
+import { CgInfo } from "react-icons/cg";
 
 import ButtonStyle1 from "./Buttons/ButtonStyle1";
 
@@ -14,6 +16,9 @@ interface VaultCardProps {
   minCollateralRation: string;
   apr: string;
   btnText: string;
+  learnMoreLink: string;
+  infoSymbol: string;
+
   btnAction: ReactEventHandler;
 }
 const VaultCard: React.FC<VaultCardProps> = ({
@@ -25,6 +30,8 @@ const VaultCard: React.FC<VaultCardProps> = ({
   minCollateralRation,
   apr,
   btnText,
+  learnMoreLink,
+  infoSymbol,
   btnAction,
 }) => {
   return (
@@ -37,8 +44,8 @@ const VaultCard: React.FC<VaultCardProps> = ({
         </div>
       </div>
       <hr className=" border-[#647594]" />
-      <div className="py-8 px-12">
-        <div className="text-lightGray">
+      <div className="flex flex-col gap-8 my-12">
+        <div className=" px-12 text-lightGray">
           <div className="flex justify-between items-center gap-3">
             <p className="font-medium text-[12px] leading-[24px]">Total value locked</p>
             <p className="font-bold text-[12px] leading-[24px]">{tvl}</p>
@@ -52,10 +59,21 @@ const VaultCard: React.FC<VaultCardProps> = ({
             <p className="font-bold text-[12px] leading-[24px]">{minCollateralRation}</p>
           </div>
         </div>
-        <p className="text-center font-medium text-[16px] leading-[32px] my-6">
+        <p className="text-center font-medium text-[16px] leading-[32px] bg-lightGray">
           APR <span className="font-bold"> {apr}</span>
         </p>
-        <ButtonStyle1 text={btnText} action={btnAction} />
+        <div className="px-24 flex items-center gap-4">
+          <CgInfo size={40} />
+          <p className="font-medium text-[12px] leading-[15px]">
+            Earn 2x <span className="font-bold">{infoSymbol}</span>points on collateral supplied.{" "}
+            <Link href={learnMoreLink}>
+              <span className="font-bold">Learn More</span>
+            </Link>
+          </p>
+        </div>
+        <div className="px-12">
+          <ButtonStyle1 text={btnText} action={btnAction} />
+        </div>
       </div>
     </div>
   );
