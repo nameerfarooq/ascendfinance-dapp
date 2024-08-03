416ae3afbd42448c782c64e6350d6b3f7fe1fb8b
SteveWilliamson1 <steve@ascendfinance.xyz>
2024-08-03 07:58:24 -0700

    Feat: Active Vault Selection Functionality

--- src/components/Buttons/ButtonStyle1.tsx
+++ src/components/Buttons/ButtonStyle1.tsx
@@ -1,7 +1,7 @@
 "use client";
 interface ButtonStyle1Props {
   text: string;
-  action: () => Promise<void>;
+  action: () => unknown;
   disabled: boolean;
 }
 const ButtonStyle1: React.FC<ButtonStyle1Props> = ({ text, action, disabled }) => {
@@ -9,7 +9,7 @@ const ButtonStyle1: React.FC<ButtonStyle1Props> = ({ text, action, disabled }) =
     <div
       onClick={async () => {
         if (!disabled) {
-          await action(); 
+          await action();
         }
       }}
       className={`w-full rounded-2xl bg-primaryColor py-3 px-8 flex items-center justify-center text-white font-bold text-[14px] leading-[28px] smooth-transition cursor-pointer hover:brightness-110 ${disabled && "aria-disabled:opacity-50 aria-disabled:cursor-not-allowed aria-disabled:hover:brightness-100"}`}
